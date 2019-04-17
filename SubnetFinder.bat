621f179431d68fedf114e5c51c9ec57628428311
Ocraz <registered@quicknet.nl>
2019-04-17 12:16:53 +0200

    Added AD DNS checks

--- SubnetFinder.bat
+++ SubnetFinder.bat
@@ -1,4 +1,24 @@
+@ECHO OFF
 SET OutputFile=IPSubnetFinder.txt
 IPCONFIG /all>%OutputFile%
 ROUTE PRINT>>%OutputFile%
-TRACERT 8.8.8.8>>%OutputFile%
+REM TRACERT 8.8.8.8>>%OutputFile%
+
+IF NOT "%1"=="" SET LookupDomain=%1
+IF NOT "%1"=="" GOTO manualDomain
+
+IF "%USERDOMAIN%"==AzureAD ECHO This computer is connected to AzureAD...
+IF "%LOGONSERVER%"=="\\%COMPUTERNAME%" ECHO This computer doesn't seem to be domain-joined...
+IF "%LOGONSERVER%"=="\\%COMPUTERNAME%" GOTO skipDomain
+IF "%USERDNSDOMAIN%"=="" ECHO This computer doesn't seem to be domain-joined...
+IF "%USERDNSDOMAIN%"=="" GOTO skipDomain
+SET LookupDomain=%USERDNSDOMAIN%
+:manualDomain
+	nslookup %LookupDomain%>>%OutputFile%
+	
+	nslookup -type=srv _kerberos._tcp.%LookupDomain%>>%OutputFile%
+	nslookup -type=srv _kpasswd._tcp.%LookupDomain%>>%OutputFile%
+	nslookup -type=srv _ldap._tcp.%LookupDomain%>>%OutputFile%
+	nslookup -type=srv _ldap._tcp.dc._msdcs.%LookupDomain%>>%OutputFile%
+	
+:skipDomain
